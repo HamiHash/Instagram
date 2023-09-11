0b758249bfb9a00385673e68a6eb7b39322d80f0
hamihash <hami.hash@yahoo.com>
2023-09-11 19:45:58 +0330

    first commit

---
+++ Instagram/Core/TabBar/TabBar.swift
@@ -0,0 +1,20 @@
+//
+//  TabBar.swift
+//  Instagram
+//
+//  Created by Hamed Hashemi on 9/11/23.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
