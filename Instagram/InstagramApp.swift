64921f8a670a4965410c859f84a8f0825d3097fd
hamihash <hami.hash@yahoo.com>
2023-09-11 17:51:46 +0330

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Hamed Hashemi on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
